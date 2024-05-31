ccbb736e875c9ca747f1e220b31feb5ed76126c7
memoryDev <memorydev2024@gmail.com>
2024-05-31 13:20:53 +0900

    상품 도메인 개발

--- src/main/java/jpabook/jpabook/domain/item/Item.java
+++ src/main/java/jpabook/jpabook/domain/item/Item.java
@@ -2,6 +2,7 @@ package jpabook.jpabook.domain.item;
 
 import jakarta.persistence.*;
 import jpabook.jpabook.domain.Category;
+import jpabook.jpabook.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -29,4 +30,23 @@ public abstract class Item {
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
 
+    //==비즈니스 로직==//
+    /**
+     * stock 증가
+     */
+    public void addStock(int quantity) {
+        stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     */
+    public void removeStock(int quantity) {
+        int restStock = stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+
+        stockQuantity = restStock;
+    }
 }
